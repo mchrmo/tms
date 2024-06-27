3a28e138a313cb5a3c09eb936a4fbeb8b0c22999
Michal Chrmo <mchrmo@gmail.com>
2024-06-27 06:40:35 +0200

    sendgrid migration

--- src/app/api/users/send-email/route.ts
+++ src/app/api/users/send-email/route.ts
@@ -1,10 +1,14 @@
 import { sendWelcomeEmail } from '@/lib/services/mail.service';
 import { auth, clerkClient } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
+import Email from 'vercel-email';
 
 export const GET = async () => {
 
     await sendWelcomeEmail('chrmo.keepsmart@gmail.com', "TEST")
+
+
+    
   
     
     return NextResponse.json({ message: "Success: email was sent" }, { status: 200 });
--- src/lib/services/mail.service.ts
+++ src/lib/services/mail.service.ts
@@ -1,6 +1,13 @@
 'use server'
 
+// TWILIO BPS68FDK5PQVZ2VLLW6UKQ3C
+import Email from "vercel-email";
+
 const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail')
+
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
 const mailService = nodemailer.createTransport({
   host: 'mail.webglobe.sk',
@@ -18,20 +25,31 @@ export async function sendWelcomeEmail(email: string, password: string) {
   console.log("Sending email to ", email);
   
 
-  const mail = await mailService.sendMail({
-      from: 'support@flexishop.online',
-      to: email,
-      replyTo: email,
-      subject: `Vitajte v systéme TMS mesta Ružomberok`,
-      html: `
-        <h2>Pre prihlásenie použite nasledovné údaje: </h2>
-        <p style="margin: 0">Email: <b>${email}</b> </p>
-        <p style="margin: 0">Heslo: <b>${password}</b> </p>
-        <p>Prosím prihláste sa tu: <a href="https://tms-six-ruddy.vercel.app/sign-in">https://tms-six-ruddy.vercel.app</a></p>
+  // const mail = await mailService.sendMail({
+  //     from: 'support@flexishop.online',
+  //     to: email,
+  //     replyTo: email,
+  //     subject: `Vitajte v systéme TMS mesta Ružomberok`,
+  //     html: `
+  //       <h2>Pre prihlásenie použite nasledovné údaje: </h2>
+  //       <p style="margin: 0">Email: <b>${email}</b> </p>
+  //       <p style="margin: 0">Heslo: <b>${password}</b> </p>
+  //       <p>Prosím prihláste sa tu: <a href="https://tms-six-ruddy.vercel.app/sign-in">https://tms-six-ruddy.vercel.app</a></p>
+  //     `,
+  // })  
 
-      `,
-  })  
 
-  console.log(mail.response);
-  
+  await sgMail.send({
+    from: 'support@flexishop.online',
+    to: email,
+    subject: `Vitajte v systéme TMS mesta Ružomberok`,
+    html: `
+      <h2>Pre prihlásenie použite nasledovné údaje: </h2>
+      <p style="margin: 0">Email: <b>${email}</b> </p>
+      <p style="margin: 0">Heslo: <b>${password}</b> </p>
+      <p>Prosím prihláste sa tu: <a href="https://tms-six-ruddy.vercel.app/sign-in">https://tms-six-ruddy.vercel.app</a></p>
+    `
+  })
+
+
 }
